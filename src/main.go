5a0414b672a669299ad48ce3fa4091a08360bad9
benjamin <benjamin@niclabs.cl>
2016-01-27 16:08:43 -0300

    Add manage of sse url

--- src/main.go
+++ src/main.go
@@ -26,7 +26,7 @@ func serversLocation(rw http.ResponseWriter, rq *http.Request) {
 	encoder.Encode(serversLocation)
 }
 
-func runSseServer(redisClient *redis.Client, serverFunction func(eventManager *sse.EventManager, client *redis.Client), url string){
+func runSseServer(redisClient *redis.Client, serverFunction func(eventManager *sse.EventManager, client *redis.Client), url string) {
 	eventManager := sse.NewEventManager()
 	go eventManager.Listen()
 	serverFunction(eventManager, redisClient)
@@ -42,8 +42,7 @@ func main() {
 
 	runSseServer(redisClient, servers.ServDataEvent, "/servData")
 	runSseServer(redisClient, servers.GeoEvent, "/geo")
-
-	//geo
+	runSseServer(redisClient, servers.TopKEvent, "/sse")
 	http.HandleFunc("/serversLocation", serversLocation)
 	http.ListenAndServe(":8080", nil)
 }
