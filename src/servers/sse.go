6dc920daabea4f972d15e83f552afbffe8e48e06
benjamin <benjamin@niclabs.cl>
2016-01-28 15:34:11 -0300

    Refactor code of big function and manage error withor panicking

--- src/servers/sse.go
+++ src/servers/sse.go
@@ -24,9 +24,11 @@ func TopKEvent(eventManager *sse.EventManager, client *redis.Client) {
 		panic(err)
 	}
 	times := []string{"60", "300", "900"}
+
 	script := "local old_jsons = redis.call('zrangebyscore', KEYS[1], '-inf' , ARGV[1]);" +
 	"redis.call('zremrangebyscore', KEYS[1], '-inf', ARGV[1]);" +
 	"return old_jsons;"
+
 	go obtainTopK(times, script, "nameCount", processTopK, topk, redisWriter, eventManager)
 
 	go obtainTopK(times, script, "malformed", processMalformed, malformed, redisWriter, eventManager)
@@ -74,10 +76,7 @@ func processTopK(topk *redis.PubSub) (orderedValues QueriesCounter, msg Message,
 func obtainTopK(times []string, script string, name string, process func(*redis.PubSub) (QueriesCounter, Message, error), redisSubscribe *redis.PubSub, redisWriter *redis.Client, eventManager *sse.EventManager){
 	for {
 		orderedValues, msg, err:= process(redisSubscribe)
-		if err != nil{
-			fmt.Println(err)
-			continue
-		}
+		manageError(err)
 		for _, seconds := range times {
 			//TODO: launch the process for every second in a different thread?
 			historicChannel := "historicNameCounts:" + msg.ServerId + ":" + seconds
@@ -86,102 +85,82 @@ func obtainTopK(times []string, script string, name string, process func(*redis.
 			multi, err := redisWriter.Watch(historicChannel,
 				serverChannel,
 				globalChannel)
-			if err != nil {
-				panic(err)
-			}
+			if manageError(err){continue}
 			now := float64(time.Now().UnixNano() / 1000000)
 			zAdd := redisWriter.ZAdd(historicChannel, redis.Z{now, orderedValues})
-			if zAdd.Err() != nil {
-				panic(zAdd.Err())
-			}
+			if manageError(zAdd.Err()){continue}
 			for i := 0; i < len(orderedValues); i++ {
-				serverZIncrBy := multi.ZIncrBy(serverChannel, float64(orderedValues[i].Counter), orderedValues[i].Query)
-				if serverZIncrBy.Err() != nil {
-					panic(serverZIncrBy.Err())
-				}
-				globalZIncrBy := multi.ZIncrBy(globalChannel, float64(orderedValues[i].Counter), orderedValues[i].Query)
-				if globalZIncrBy.Err() != nil {
-					panic(globalZIncrBy.Err())
-				}
+				zIncrBy(multi, serverChannel, orderedValues[i])
+				zIncrBy(multi, globalChannel, orderedValues[i])
 			}
-			_, err = multi.Exec(func() error {
-				return nil
-			})
-			if err != nil {
-				panic(err)
-			}
-
+			execMulti(multi)
 			floatSeconds, err := strconv.ParseFloat(seconds, 64)
-			if err != nil {
-				panic(err)
-			}
+			if manageError(err){continue}
 			timeRange := strconv.FormatFloat(now - floatSeconds * 1000, 'g', -1, 64)
 			scriptEval := redisWriter.Eval(script, []string{historicChannel}, []string{timeRange})
-			if scriptEval.Err() != nil {
-				panic(scriptEval.Err())
-			}
+			if manageError(scriptEval.Err()){continue}
 			s := scriptEval.Val().([]interface{})
 			for i := 0; i < len(s); i++ {
 				var oldQueries []interface{}
-				if err = json.Unmarshal([]byte(s[i].(string)), &oldQueries); err != nil {
-					fmt.Println("Couldn't unmarshall values")
-					continue
-				}
+				err = json.Unmarshal([]byte(s[i].(string)), &oldQueries)
+				if manageError(err){continue}
 				for j := 0; j < len(oldQueries); j++ {
 					url := reflect.ValueOf(oldQueries[j]).Index(0).Interface().(string)
 					counter := -1 * reflect.ValueOf(oldQueries[j]).Index(1).Interface().(float64)
-					if serverZIncrBy := multi.ZIncrBy(serverChannel, counter, url); serverZIncrBy.Err() != nil {
-						panic(serverZIncrBy.Err())
-					}
-					if globalZIncrBy := multi.ZIncrBy(globalChannel, counter, url); globalZIncrBy.Err() != nil {
-						panic(globalZIncrBy.Err())
-					}
+					serverZIncrBy := multi.ZIncrBy(serverChannel, counter, url)
+					if manageError(serverZIncrBy.Err()){continue}
+					globalZIncrBy := multi.ZIncrBy(globalChannel, counter, url)
+					if manageError(globalZIncrBy.Err()){continue}
 				}
 				multi.ZRemRangeByScore(serverChannel, "-inf", "0")
 				multi.ZRemRangeByScore(globalChannel, "-inf", "0")
 			}
-			if _, err := multi.Exec(func() error {
-				return nil
-			}); err != nil {
-				panic(err)
-			}
+			execMulti(multi)
 			serverTopK := multi.ZRevRangeWithScores(serverChannel, 0, 4)
-			if serverTopK.Err() != nil {
-				panic(err)
-			}
+			if manageError(serverTopK.Err()){continue}
 			globalTopK := multi.ZRevRangeWithScores(globalChannel, 0, 4)
-			if globalTopK.Err() != nil {
-				panic(err)
-			}
-			_, err = multi.Exec(func() error {
-				return nil
-			})
-			if err != nil {
-				panic(err)
-			}
-			serverTopKValues := make([][]string, len(serverTopK.Val()))
-			for i, value := range serverTopK.Val() {
-				serverTopKValues[i] = []string{value.Member.(string), strconv.FormatFloat(value.Score, 'f', -1, 64)}
-			}
-			serverTopkMessage := Message{ServerId:msg.ServerId, Type: name + "TopK:" + msg.ServerId + ":" + seconds,
-				TimeStamp:time.Now().UnixNano() / 1000000, Payload:serverTopKValues}
-			serverOutputMsg, err := serverTopkMessage.MarshalJSON()
-			if err != nil {
-				panic(err)
-			}
-			globalTopKValues := make([][]string, len(globalTopK.Val()))
-			for i, value := range globalTopK.Val() {
-				globalTopKValues[i] = []string{value.Member.(string), strconv.FormatFloat(value.Score, 'f', -1, 64)}
-			}
-			globalTopkMessage := Message{ServerId:"GLOBAL", Type: name + "TopK:GLOBAL:" + seconds,
-				TimeStamp:time.Now().UnixNano() / 1000000, Payload:globalTopKValues}
-			globalOutputMsg, err := globalTopkMessage.MarshalJSON()
-			if err != nil {
-				panic(err)
-			}
-			eventManager.InputChannel <- []byte(serverOutputMsg)
-			eventManager.InputChannel <- []byte(globalOutputMsg)
+			if manageError(globalTopK.Err()){continue}
+			execMulti(multi)
+			eventManager.InputChannel <- []byte(getOutputMessage(serverTopK.Val(), msg.ServerId, name, seconds))
+			eventManager.InputChannel <- []byte(getOutputMessage(globalTopK.Val(), "GLOBAL", name, seconds))
 			multi.Close()
 		}
 	}
+}
+
+func zIncrBy(multi *redis.Multi, channel string, qc QueryCounter){
+	incrBy := multi.ZIncrBy(channel, float64(qc.Counter), qc.Query)
+	if incrBy.Err() != nil {
+		panic(incrBy.Err())
+	}
+}
+
+func getOutputMessage(values []redis.Z, serverId string, name string, seconds string) []byte{
+	serverTopKValues := make([][]string, len(values))
+	for i, value := range values {
+		serverTopKValues[i] = []string{value.Member.(string), strconv.FormatFloat(value.Score, 'f', -1, 64)}
+	}
+	serverTopkMessage := Message{ServerId:serverId, Type: name + "TopK:" + serverId + ":" + seconds,
+		TimeStamp:time.Now().UnixNano() / 1000000, Payload:serverTopKValues}
+	outputMessage, err := serverTopkMessage.MarshalJSON()
+	if err != nil {
+		panic(err)
+	}
+	return outputMessage
+}
+
+func execMulti(multi *redis.Multi){
+	if _, err := multi.Exec(func() error {
+		return nil
+	}); err != nil {
+		fmt.Println(err)//TODO:logger
+	}
+}
+
+func manageError(err error) bool{
+	if err != nil{
+		fmt.Println(err)
+		return true
+	}
+	return false
 }
