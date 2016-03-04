346be30dae489f660ced10e1148fc885e5c52493
benjamin <benjamin@niclabs.cl>
2016-03-04 17:41:16 -0300

    Created documentation.

--- src/sse/sse.go
+++ src/sse/sse.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-//TODO:
+//Listener struct stores an OutPut channel to write messages after processing them with the ProcessFunction.
 type Listener struct{
 	//Channel to where the processed data will be written
 	OutputChannel chan []byte
@@ -14,6 +14,7 @@ type Listener struct{
 	processFunction func(eventData [] byte) []byte
 }
 
+//NewListener initialize a Listener with the specified process function returning a pointer to the Listener.
 func NewListener(processFunction func (eventData [] byte) []byte) *Listener {
 	listener := new(Listener)
 	listener.OutputChannel = make(chan []byte)
@@ -21,7 +22,8 @@ func NewListener(processFunction func (eventData [] byte) []byte) *Listener {
 	return listener
 }
 
-//TODO:
+//EventManager struct that has a map of pointer of Listener to booleans to check if they are being used and the channel
+//where to those Listeners will write. //TODO is the map really necessary?
 type EventManager struct{
 	//Set of Listeners to which the events will be processed
 	listeners map[*Listener]bool
@@ -30,7 +32,7 @@ type EventManager struct{
 	InputChannel chan []byte
 }
 
-//TODO
+//NewEventManager initialize an EventManager with default values and return a pointer to the EventManager.
 func NewEventManager() *EventManager {
 	eventManager := new(EventManager)
 	eventManager.listeners = make(map[*Listener]bool)
@@ -38,16 +40,18 @@ func NewEventManager() *EventManager {
 	return eventManager
 }
 
-//TODO:
+//AddListener set the values of the pointer of a listener to true (so it's being used now).
 func (event *EventManager) AddListener( listener *Listener){
 	event.listeners[listener] = true
 }
 
+//RemoveListener removes the Listener of the map.
 func (event *EventManager) RemoveListener(listener *Listener) {
 	delete(event.listeners, listener)
 }
 
-//TODO:
+//Listen starts a never ending for that reads a message of input channel and, for every Listener in listeners,
+// writes the message in the Listener OutputChannel.
 func (event *EventManager) Listen(){
 	for{
 		data := <-event.InputChannel
@@ -57,11 +61,14 @@ func (event *EventManager) Listen(){
 	}
 }
 
+//SSEServer struct has an EventManager where to read messages and a function to process the message and
+//then writes the message to a HTML5 SSE.
 type SSEServer struct {
 	eventManager *EventManager
 	processFunction func(eventData [] byte) []byte
 }
 
+//NewSSEServer initialize a SSESever with the default values.
 func NewSSEServer(eventManager *EventManager, processFunction func(in []byte) []byte) *SSEServer{
 	sseServer := new(SSEServer)
 	sseServer.eventManager = eventManager
@@ -70,12 +77,9 @@ func NewSSEServer(eventManager *EventManager, processFunction func(in []byte) []
 	return sseServer
 }
 
-
+//ServeHTTP writes the messages of the SSEServer OutputChannel and writes it to an HTML5 SSE.
 func (server *SSEServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-
-
 	// Make sure that the writer supports flushing.
-	//
 	flusher, ok := writer.(http.Flusher)
 	if !ok {
 		http.Error(writer, "Streaming unsupported!", http.StatusInternalServerError)
@@ -95,7 +99,6 @@ func (server *SSEServer) ServeHTTP(writer http.ResponseWriter, request *http.Req
 		server.eventManager.RemoveListener(listener)
 		close(listener.OutputChannel)
 	}()
-
 
 	// Listen to connection close and un-register messageChan
 	notify := writer.(http.CloseNotifier).CloseNotify()
