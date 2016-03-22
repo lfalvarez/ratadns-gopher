4444c70ad9eca40d92781997988a2c5273ed4742
Francisco Cifuentes <francisco@niclabs.cl>
2016-03-22 16:27:24 -0300

    Delete original Go Code

--- util/location.go
+++
@@ -1,9 +0,0 @@
-package util
-
-//Location struct to marshal the location of QueriesSummary.
-//The struct has a longitude, a latitude and the name of the country of that location.
-type Location struct {
-	Longitude   float64 `json:"longitude"`
-	Latitude    float64 `json:"latitude"`
-	CountryName string `json:"country_name"`
-}
