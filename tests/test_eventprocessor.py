1856a404a17f82b34d36591ace3c85292a21d396
Francisco Cifuentes <francisco@niclabs.cl>
2016-05-05 16:52:18 -0300

    Add comments on TestEventProcessor. Refactor on order of instructions on setUp method.

--- tests/test_eventprocessor.py
+++ tests/test_eventprocessor.py
@@ -34,31 +34,45 @@ class TestServerDataEventProcessor(unittest.TestCase):
 
 class TestEventProcessor(unittest.TestCase):
     def setUp(self):
-        self.redis_mock = MagicMock(spec=redis.StrictRedis)
+        # First we set the data that will be "produced" by the redis pubsub listener.
+        self.data = ["data to consume", "data not to consume"]
+        self.data_to_consume = self.data[0]
+        self.data_not_to_consume = self.data[1]
+
+        # Second we set the redis pubsub listener
+        self.redis_mock = MagicMock(spec=redis.StrictRedis) # Redis client
+        self.redis_mock.pubsub().listen = \
+            MagicMock(return_value=list(map(lambda d: {'data': "\"{}\"".format(d).encode()}, self.data)))
+
+        # Third we create the process function (that will replace the process method
+        # from the EventProcessor abstract class
+        self.process_side_effects = list(zip([True, False], self.data))
+        self.process_mock = MagicMock(side_effect = self.process_side_effects)
+
+        # Fourth we set the consumers to be registered
         self.consumer1_mock = MagicMock(spec=EventConsumer) # consumer that will register
         self.consumer2_mock = MagicMock(spec=EventConsumer) # consumer that will register and then unregister
-        self.data_to_consume = "data to consume"
-        self.data_not_to_consume = "data not to consume"
-        self.process_side_effects = [(True, self.data_to_consume),
-                                     (False, self.data_not_to_consume)]
-        self.process_mock = MagicMock(side_effect = self.process_side_effects)
-        self.redis_mock.pubsub().listen = \
-            MagicMock(return_value=[{'data': "\"{}\"".format(self.data_to_consume).encode()},
-                                    {'data': "\"{}\"".format(self.data_not_to_consume).encode()}])
 
     def test_calls_to_redis_methods(self):
+        """
+        Test case when we call the EventProcessor subscribe method, it has to
+        call the redis pubsub subscribe method.
+        """
         ep = EventProcessor(self.redis_mock)
         self.redis_mock.pubsub.assert_any_call()
         ep.subscribe("topk")
         self.redis_mock.pubsub().subscribe.assert_called_once_with("topk")
 
     def test_process_is_called_with_all_data(self):
+        """
+        Test case to check that process uses all the data received from the redis.pubsub().listen
+        """
         ep = EventProcessor(self.redis_mock)
         ep.register_consumer(self.consumer1_mock)
         ep.process = self.process_mock
         ep.run()
         # process was called two times, each time called with different data
-        ep.process.assert_has_calls([call(self.data_to_consume), call(self.data_not_to_consume)])
+        ep.process.assert_has_calls(map(lambda d: call(d), self.data))
 
 
     def test_one_consumer_registered(self):
@@ -72,12 +86,17 @@ class TestEventProcessor(unittest.TestCase):
         ep.register_consumer(self.consumer1_mock)
         ep.process = self.process_mock
         ep.run()
-        # process was called two times, each time called with different data
-        ep.process.assert_has_calls([call(self.data_to_consume), call(self.data_not_to_consume)])
+
         # consume was only called exactly once AND that time was with data_to_consume
         self.consumer1_mock.consume.assert_called_once_with(self.data_to_consume)
 
     def test_two_consumers_registered_and_one_unregistered_later(self):
+        """
+        Test case when two consumer are registered. Then the run method of EventProcessor is called.
+        After that, one of the consumers unregisters and the run method is called once again.
+        Finally is checked that the consumer that never unregistered was called twice, and the consumer
+        that unregistered was only call once.
+        """
         ep = EventProcessor(self.redis_mock)
         ep.register_consumer(self.consumer1_mock)
         ep.register_consumer(self.consumer2_mock)
@@ -91,6 +110,7 @@ class TestEventProcessor(unittest.TestCase):
         ep.unregister_consumer(self.consumer2_mock)
         ep.run()
 
-        self.consumer1_mock.consume.assert_has_calls([call(self.data_to_consume),
-                                                     call(self.data_to_consume)])
+        # As we only unregistered consumer2_mock, consumer1_mock should have been called again
+        #
+        self.consumer1_mock.consume.assert_has_calls([call(self.data_to_consume)]*2)
         self.consumer2_mock.consume.assert_called_once_with(self.data_to_consume)
