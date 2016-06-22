be4342eadcb7cf799744ff6a19a0b7a4f05e82f8
Francisco Cifuentes <francisco@niclabs.cl>
2016-06-22 17:02:29 -0400

    Add MovingWindow Test

--- tests/test_eventprocessor.py
+++ tests/test_eventprocessor.py
@@ -1,12 +1,15 @@
 import datetime
 import random
-import redis
-import redis.client
 import unittest
 import time as Time
 
-from gopher.eventprocessor import EventConsumer, ServerDataEventProcessor, EventProcessor, TopQNamesEventProcessor, \
-    QueriesSummaryEventProcessor
+from gopher.eventprocessor import \
+        EventConsumer, \
+        MovingWindow, \
+        ServerDataEventProcessor, \
+        EventProcessor, \
+        TopQNamesEventProcessor, \
+        QueriesSummaryEventProcessor
 
 
 class TestEventConsumer(unittest.TestCase):
@@ -18,20 +21,40 @@ class TestEventConsumer(unittest.TestCase):
         self.assertEqual(input_value, output_value)
 
 
-class TestEventProcessor(unittest.TestCase):
+# class TestEventProcessor(unittest.TestCase):
+#     def setUp(self):
+#         pass
+# 
+#     def test_register_consumer(self):
+#         pass
+
+class TestMovingWindow(unittest.TestCase):
     def setUp(self):
-        pass
+        self.moving_window = MovingWindow()
 
-    def test_register_consumer(self):
-        pass
+    def test_add_10_items_and_get(self):
+        test_list = [i for i in range(0,10)]
+        for i in test_list:
+            self.moving_window.add_item(i, i)
+
+        items = self.moving_window.get_items_after_limit(-1)
+        self.assertListEqual(items, test_list)
+
+        items = self.moving_window.get_items_after_limit(4)
+        self.assertListEqual(items, [i for i in range(5, 10)])
+
+        items = self.moving_window.get_items_after_limit(-1)
+        self.assertListEqual(items, test_list)
+
+    def test_add_10_items_remove_and_get(self):
+        for i in range(0,10):
+            self.moving_window.add_item(i, i)
+        self.moving_window.remove_old_data(4)
+
+        items = self.moving_window.get_items_after_limit(-1)
+        self.assertListEqual(items, [i for i in range(5, 10)])
 
 
-
-
-
-# class TestMovingWindow(unittest.TestCase):
-#
-#
 # class TestWindowedEventProcessor(unittest.TestCase):
 #
 #
