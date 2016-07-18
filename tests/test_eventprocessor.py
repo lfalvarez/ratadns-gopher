3ce79821dbb29861b3b434aae957e85793df917e
Camilo Gomez <cagomeznu@gmail.com>
2016-07-18 16:32:05 -0400

    Test get 2 best elements from complex_list

--- tests/test_eventprocessor.py
+++ tests/test_eventprocessor.py
@@ -1,16 +1,11 @@
 import random
 import unittest
 
-
 from gopher.eventprocessor import \
-        EventConsumer, \
-        MovingWindow, \
-        ServerDataEventProcessor, \
-        EventProcessor, \
-        TopQNamesEventProcessor, \
-        QueriesSummaryEventProcessor, \
-        hex2ip, \
-        get_topk
+    EventConsumer, \
+    MovingWindow, \
+    hex2ip, \
+    get_topk
 
 
 class TestEventConsumer(unittest.TestCase):
@@ -34,7 +29,7 @@ class TestMovingWindow(unittest.TestCase):
         self.moving_window = MovingWindow()
 
     def test_add_10_items_and_get(self):
-        test_list = [i for i in range(0,10)]
+        test_list = [i for i in range(0, 10)]
         for i in test_list:
             self.moving_window.add_item(i, i)
 
@@ -48,7 +43,7 @@ class TestMovingWindow(unittest.TestCase):
         self.assertListEqual(items, test_list)
 
     def test_add_10_items_remove_and_get(self):
-        for i in range(0,10):
+        for i in range(0, 10):
             self.moving_window.add_item(i, i)
         self.moving_window.remove_old_data(4)
 
@@ -71,22 +66,35 @@ class TestHex2IP(unittest.TestCase):
 class TestGetTopK(unittest.TestCase):
     def setUp(self):
         self.simple_list = [1, 2, 3, 4, 5]
-        self.identity = lambda x: x
+        self.identity_fn = lambda x: x
+        self.item1 = {"arg1": 1, "arg2": 1}
+        self.item2 = {"arg1": 2, "arg2": 3}
+        self.item3 = {"arg1": 3, "arg2": 2}
+        self.item4 = {"arg1": 4, "arg2": 4}
+        self.item5 = {"arg1": 1, "arg2": 5}
+        self.complex_list = [self.item1, self.item2, self.item3, self.item4, self.item5]
 
-    def test_get_0_elements(self):
+    def test_get_0_elements_simple(self):
         k = 0
-        result_list = get_topk(self.simple_list, k, self.identity)
+        result_list = get_topk(self.simple_list, k, self.identity_fn)
         self.assertListEqual(result_list, [])
 
-    def test_get_more_elements_than_size(self):
+    def test_get_more_elements_than_size_simple(self):
         k = len(self.simple_list) + 1
-        result_list = get_topk(self.simple_list, k, self.identity)
+        result_list = get_topk(self.simple_list, k, self.identity_fn)
         self.assertListEqual(result_list, sorted(self.simple_list, reverse=True))
 
-    def test_get_3_elements(self):
+    def test_get_3_elements_simple(self):
         k = 3
-        result_list = get_topk(self.simple_list, k, self.identity)
+        result_list = get_topk(self.simple_list, k, self.identity_fn)
         self.assertListEqual(result_list, [5, 4, 3])
+
+    def test_get_2_elements_complex(self):
+        k = 2
+        key_fn = lambda x: x["arg2"]
+        result_list = get_topk(self.complex_list, k, key_fn)
+        self.assertListEqual(result_list, [self.item5, self.item4])
+
 
 # class TestWindowedEventProcessor(unittest.TestCase):
 #
